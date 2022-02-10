1aa3609a6c7e85801200888ac0910d16e68b3e5e
Giuliano Beltrami <juka2027@live.com>
2022-02-10 17:06:59 -0300

    Removing styles from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Details from './components/Details/Details';
-import useStyles from './styles';
 
 const App: Function = (): JSX.Element => {
-  const classes = useStyles();
 
   return (
     <div>
-      <Grid container spacing={0} alignItems="center" justify="center" style={classes.grid}>
+      <Grid container spacing={0} alignItems="center" justify="center" style={{height: '100vh'}}>
         <Grid>
-          
+
         </Grid>
       </Grid>
     </div>
--- src/styles.tsx
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from '@material-ui/core';
-
-export default makeStyles(() => ({
-    grid:{
-        height:'100vh'
-    }
-}));
